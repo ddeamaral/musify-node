030c803043273041c5e52b0eab70c0062f2b8aee
Dylan DeAmaral <deamaral.dylan@gmail.com>
2017-08-07 18:01:50 -0400

    added a default/temporary layout from an express/ejs tutorial.  This kind of altered the layout for the project

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 var express = require('express');
 let app = express();
 
+
+
+// turn on view engine
+app.set('view engine', 'ejs');
+
+// set the static directories that are browsable for client
 app.use(express.static('public'));
-app.use(express.static('src/Views'));
 
 
-app.get('/', function(req, res){
-    res.send('Home page TBD');
+app.get('/', function(req, res) {
+    res.render('pages/index');
 });
 
+// about page 
+app.get('/about', function(req, res) {
+    res.render('pages/about');
+});
 
 app.listen(3000, 'localhost', function(){
     console.log("listening for requests at localhost:3000/");
